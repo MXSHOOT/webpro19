a86833759f5cfc030830e448a9890b29d76d2b43
MXSHOOT <farisarrasyid78@gmail.com>
2020-11-03 11:18:09 +0700

    project praktikum 3

---
+++ praktikum05/js/main.js
@@ -0,0 +1,17 @@
+//variabel
+var test;
+test = 10;
+//langsung kebaca integral
+
+// alert(test)
+
+var score = 90
+var score2 = 95
+var score3 = 100
+var score4 = 95
+
+//array
+var nilai = [90,95,100,95]
+nilai[2]
+
+alert(nilai[2])
